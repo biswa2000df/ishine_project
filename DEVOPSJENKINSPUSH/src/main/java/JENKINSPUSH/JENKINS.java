8b5e1778f67d33d42e4f76923318181c05adc2af
biswajit <biswajit.sahoo@apmosys.cmo>
2023-04-21 17:50:48 +0530

    This is my first commit

---
+++ DEVOPSJENKINSPUSH/src/main/java/JENKINSPUSH/JENKINS.java
@@ -0,0 +1,19 @@
+package JENKINSPUSH;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class JENKINS {
+
+	public static void main(String[] args) {
+	
+		
+		System.out.println("My name is biswajit sahoo");
+		
+
+	}
+
+}
